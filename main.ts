731f366b82075b561368985e31213acfb8b6fb11
karlpederolesen <96655854+karlpederolesen@users.noreply.github.com>
2021-12-26 17:28:51 +0100

    Opdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 let temperatur = 0
 let antal = 0
 basic.forever(function () {
-    temperatur = input.temperature()
+    temperatur = input.temperature() - 2
     antal = Math.constrain(Math.floor(temperatur / 2 - 9), 0, 5)
     for (let indeks = 0; indeks <= 4; indeks++) {
         if (indeks <= antal - 1) {
